5c3e23eca3ff00657de2252a3674ad7d7f6d4a3e
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-05 12:58:55 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -20,24 +20,24 @@ def data_visualization():
     col=list(dataset.columns)
     col.remove("isFraud")
     print(col)
-    for i in col:
-        fig = px.box(dataset, y=i)
-        fig.update_layout(template='plotly_dark')
-        #fig.update_layout(plot_bgcolor = "plotly_dark")
-        fig.update_xaxes(showgrid=False,zeroline=False)
-        fig.update_yaxes(showgrid=False,zeroline=False)
-        # fig.show()
-        fig.write_image(f"{i}.jpg")
-        # a.append(fig)
+    # for i in col:
+    #     fig = px.box(dataset, y=i)
+    #     fig.update_layout(template='plotly_dark')
+    #     #fig.update_layout(plot_bgcolor = "plotly_dark")
+    #     fig.update_xaxes(showgrid=False,zeroline=False)
+    #     fig.update_yaxes(showgrid=False,zeroline=False)
+    #     # fig.show()
+    #     fig.write_image(f"{i}.jpg")
+    #     # a.append(fig)
 
-    for i in col:
-        fig = px.histogram(dataset, y=i, marginal="box")
-        fig.update_layout(template='plotly_dark')
-        fig.update_xaxes(showgrid=False, zeroline=False)
-        fig.update_yaxes(showgrid=False, zeroline=False)
-        # fig.show()
-        fig.write_image(f"{i}_hist.jpg")
-        # a.append(fig)
+    # for i in col:
+    #     fig = px.histogram(dataset, y=i, marginal="box")
+    #     fig.update_layout(template='plotly_dark')
+    #     fig.update_xaxes(showgrid=False, zeroline=False)
+    #     fig.update_yaxes(showgrid=False, zeroline=False)
+    #     # fig.show()
+    #     fig.write_image(f"{i}_hist.jpg")
+    #     # a.append(fig)
         
     # for i in col:
     #     fig = ff.create_distplot(dataset, y=i, marginal="box")
