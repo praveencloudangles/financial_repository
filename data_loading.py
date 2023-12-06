fd42de47bd79c4fbce33d194328d9f5b5906f490
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-06 17:00:53 +0530

    Update data_loading.py

--- data_loading.py
+++ data_loading.py
@@ -5,10 +5,9 @@ import pandas as pd
 def data_loading():
     client = boto3.client('s3')
 
-    path = "https://mlops-storage1.s3.amazonaws.com/PS_20174392719_1491204439457_log.csv"
+    path = "risk_dataset.csv"
 
     df = pd.read_csv(path)
     print(df.head())
-
     return df
 data_loading()
