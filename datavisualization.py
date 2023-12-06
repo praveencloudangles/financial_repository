1a9d9c585bf11744151af04e03643dff6e401dea
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2023-12-06 16:06:28 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -20,24 +20,24 @@ def data_visualization():
     col=list(dataset.columns)
     col.remove("isFraud")
     print(col)
-    for i in col:
-        fig = px.box(dataset, y=i)
-        fig.update_layout(template='plotly_dark')
-        #fig.update_layout(plot_bgcolor = "plotly_dark")
-        fig.update_xaxes(showgrid=False,zeroline=False)
-        fig.update_yaxes(showgrid=False,zeroline=False)
-        # fig.show()
-        fig.write_image(f"{i}.jpg")
-        # a.append(fig)
+    # for i in col:
+    #     fig = px.box(dataset, y=i)
+    #     fig.update_layout(template='plotly_dark')
+    #     #fig.update_layout(plot_bgcolor = "plotly_dark")
+    #     fig.update_xaxes(showgrid=False,zeroline=False)
+    #     fig.update_yaxes(showgrid=False,zeroline=False)
+    #     # fig.show()
+    #     fig.write_image(f"{i}.jpg")
+    #     # a.append(fig)
 
-    for i in col:
-        fig = px.histogram(dataset, y=i, marginal="box")
-        fig.update_layout(template='plotly_dark')
-        fig.update_xaxes(showgrid=False, zeroline=False)
-        fig.update_yaxes(showgrid=False, zeroline=False)
-        # fig.show()
-        fig.write_image(f"{i}_hist.jpg")
-        # a.append(fig)
+    # for i in col:
+    #     fig = px.histogram(dataset, y=i, marginal="box")
+    #     fig.update_layout(template='plotly_dark')
+    #     fig.update_xaxes(showgrid=False, zeroline=False)
+    #     fig.update_yaxes(showgrid=False, zeroline=False)
+    #     # fig.show()
+    #     fig.write_image(f"{i}_hist.jpg")
+    #     # a.append(fig)
         
     # for i in col:
     #     fig = ff.create_distplot(dataset, y=i, marginal="box")
@@ -48,15 +48,15 @@ def data_visualization():
     #     fig.write_image(f"{i}_displot.jpg")
     #     # a.append(fig)
     
-    df=dataset.drop("isFraud",axis=1)
-    y=df.corr().columns.tolist()
-    z=df.corr().values.tolist()
-    z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
-    fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
-    fig.update_layout(template='plotly_dark')
-    # fig.show()
-    fig.write_image("img.jpg")
-    # a.append(fig)
+    # df=dataset.drop("isFraud",axis=1)
+    # y=df.corr().columns.tolist()
+    # z=df.corr().values.tolist()
+    # z_text = np.around(z, decimals=4) # Only show rounded value (full value on hover)
+    # fig = ff.create_annotated_heatmap(z,x=y,y=y,annotation_text=z_text,colorscale=px.colors.sequential.Cividis_r,showscale=True)
+    # fig.update_layout(template='plotly_dark')
+    # # fig.show()
+    # fig.write_image("img.jpg")
+    # # a.append(fig)
     
     return dataset
 
